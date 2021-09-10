7b83161a37e748111a3374714a47a7ba1db07a4a
tom <anytable@gmail.com>
2021-09-10 21:58:37 +0800

    vercel build

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	preprocess: preprocess(),
 	kit: {
+		adapter: vercel(),
 		target: '#svelte',
 		ssr:true,
 	}
