322d1371b448cd453d87034150aaebc979546449
tom <anytable@gmail.com>
2021-09-15 02:17:47 +0800

    configure

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
 /** @type {import('@sveltejs/kit').Config} */
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+const a={}
+let i=10
+function uniqCssName(n){
+	if(a[n]){
+		return a[n]
+	}else {
+        return  a[n]='_'+((i++).toString(36))
+	}
+}
+
 const config = {
 	compilerOptions:{
-		cssHash:({hash, css, name, filename })=>`_${hash(css)}`
+		cssHash:({hash, css, name, filename })=>uniqCssName(hash(css))
 	},
 	preprocess: preprocess(),
 	kit: {
