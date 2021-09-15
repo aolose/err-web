bdeae44d1ec822c0b62995b7b0be48f8af5d26f2
tom <anytable@gmail.com>
2021-09-16 01:25:53 +0800

    node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,42 +1,47 @@
 /** @type {import('@sveltejs/kit').Config} */
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from '@sveltejs/adapter-node';
 
 import fs from 'fs'
+
 let nm = {}
-const cssCacheFile="./cssCache.json"
+const cssCacheFile = "./cssCache.json"
 try {
-	nm=JSON.parse( fs.readFileSync(cssCacheFile).toString())
-}catch (e){}
-let i=10,t
-const b={}
-function uniqCssName(n){
-	if(nm[n]){
-		return b[n]=nm[n]
-	}else {
-		clearTimeout(t)
-		t=setTimeout(function () {
-			fs.writeFileSync(cssCacheFile,JSON.stringify(b))
-		},300)
-        return b[n]='_'+((i++).toString(36))
-	}
+    nm = JSON.parse(fs.readFileSync(cssCacheFile).toString())
+} catch (e) {
+}
+let i = 10, t
+const b = {}
+
+function uniqCssName(n) {
+    if (nm[n]) {
+        return b[n] = nm[n]
+    } else {
+        clearTimeout(t)
+        t = setTimeout(function () {
+            fs.writeFileSync(cssCacheFile, JSON.stringify(b))
+        }, 300)
+        return b[n] = '_' + ((i++).toString(36))
+    }
 }
 
 const config = {
-	compilerOptions:{
-		cssHash:({hash, css, name, filename })=>uniqCssName(hash(css))
-	},
-	preprocess: preprocess(),
-	kit: {
-		adapter: adapter(),
-		target: '#err',
-		ssr:true,
-		vite: {
-			define: {
-				'process.env': process.env,
-			},
-		},
-	}
+    compilerOptions: {
+        cssHash: ({hash, css, name, filename}) => uniqCssName(hash(css))
+    },
+    preprocess: preprocess(),
+    kit: {
+        target: '#err',
+        adapter: adapter({
+            precompress: true,
+        }),
+        ssr: true,
+        vite: {
+            define: {
+                'process.env': process.env,
+            },
+        },
+    }
 };
 
 export default config;
