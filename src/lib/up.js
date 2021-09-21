8dfe654790cb6c7e3066b9c30c6c22d4473ce34b
tom <anytable@gmail.com>
2021-09-21 19:14:28 +0800

    fix Extension upload

--- src/lib/up.js
+++ src/lib/up.js
@@ -1,13 +1,16 @@
-let host, title, cur, total, nm, tp, token
+let host, key, cur, total, nm, tp, token
 onmessage = async function ({data, target}) {
     if (typeof data === "string") {
         if (data[0] === '_') {
             token = data.substr(1)
-        } else [host, title, cur, total, nm, tp] = data.split(',')
+        } else [host, key, cur, total, nm, tp] = data.split(',')
     } else {
         const fd = new FormData();
-        if (nm) fd.append('name', nm)
-        fd.append('title', title)
+        if (nm) {
+            fd.append('tp', tp)
+            fd.append('nm', nm)
+        }
+        fd.append('key', key)
         fd.append('part', cur)
         fd.append('total', total)
         fd.append('data', data)
