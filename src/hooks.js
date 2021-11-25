f34d2f1b9f882f5f150c2b48ed2bf6cbaebefcae
tom <anytable@gmail.com>
2021-11-25 12:22:07 +0800

    new request

--- src/hooks.js
+++ src/hooks.js
@@ -2,17 +2,14 @@ import cookie from 'cookie';
 import {browser} from "$app/env";
 
 export async function externalFetch(request) {
-    const headers = {}
-    for (let pair of request.headers.entries()) {
-        headers[pair[0]] = pair[1];
-    }
     const {url} = request;
-    return await fetch(
-        browser ? url : url.replace(import.meta.env.VITE_API_CLI, import.meta.env.VITE_API_SRV),
-        {
-            ...request,
-            headers,
-        })
+    return await fetch(new Request(
+        browser ? url :
+            url.replace(
+                import.meta.env.VITE_API_CLI,
+                import.meta.env.VITE_API_SRV
+            ), request
+    ))
 }
 
 
